e0879f53b68b16cb26e1a18bfc82422ed0acae7d
Satetheus <chriswarrenmcculloughwork@gmail.com>
2020-01-12 20:13:05 -0700

    updated filesize limit in constants.php

--- constants.php
+++ constants.php
@@ -305,7 +305,7 @@ define( 'AI1WM_SUPPORT_EMAIL', 'support@servmask.com' );
 // =================
 // = Max File Size =
 // =================
-define( 'AI1WM_MAX_FILE_SIZE', 2 << 28 );
+define( 'AI1WM_MAX_FILE_SIZE', 2 << 34 );
 
 // ==================
 // = Max Chunk Size =
